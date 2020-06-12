0b04c2eae5ff3bfbbed55da47131f03ffb992ba4
Toprak Koç <hello@toprak.dev>
2020-06-12 19:42:13 +0300

    Minor changes for further steps

--- src/Containers/Main/Table.js
+++ src/Containers/Main/Table.js
@@ -4,9 +4,13 @@ import RewardItem from '../../Components/Table/RewardItem';
 import CategoryItem from '../../Components/Table/CategoryItem';
 
 const Table = () => {
-	const [test, setTest] = useState('Test');
-	const [rewards, setRewards] = useState(['R1', 'R2', 'R3', 'R4']);
-	const [categories, setCategories] = useState(['C1', 'C2', 'C3', 'C4']);
+	const rewards = ['R1', 'R2', 'R3', 'R4'];
+	const [categories, setCategories] = useState({
+		'C1': [], 
+		'C2': [], 
+		'C3': [], 
+		'C4': []
+	});
 
 	return (
 		<div className='table'>
@@ -14,8 +18,8 @@ const Table = () => {
 				<span className='title' >Rewards</span>
 				<div className='rewardItemsWrapper'>
 					{
-						rewards.map((value, index) => {
-							return <RewardItem key={index} name={value} />
+						rewards.map((reward, rewardIndex) => {
+							return <RewardItem key={rewardIndex} name={reward} />
 						})
 					}
 				</div>
@@ -24,8 +28,10 @@ const Table = () => {
 				<span className='title' >Categories</span>
 				<div  className='categoryItemsWrapper'>
 					{
-						categories.map((value, index) => {
-							return <CategoryItem key={index} name={value}/>
+						Object.keys(categories).map((categoryName, categoryIndex) => {
+							return (
+								<CategoryItem key={categoryIndex} name={categoryName}/>
+							);
 						})
 					}
 				</div>
