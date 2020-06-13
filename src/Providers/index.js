76169e7c086bad14a6508f2183cf02b4cad6d93d
Toprak Koç <hello@toprak.dev>
2020-06-13 03:13:44 +0300

    Added Category Edit Provider Into The App

--- src/Providers/index.js
+++ src/Providers/index.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { CategoryEditProvider } from "./CategoryEditProvider";
 import { DragAndDropProvider } from "./DragAndDropProvider";
 
-export const Providers = ({ children }) => {
-  return <DragAndDropProvider> {children} </DragAndDropProvider>;
-};
+export const Providers = ({ children }) => (
+	<CategoryEditProvider>
+		<DragAndDropProvider> {children} </DragAndDropProvider>
+	</CategoryEditProvider>
+);
